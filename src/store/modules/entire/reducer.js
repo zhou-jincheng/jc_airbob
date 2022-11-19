be023dfd81cf62f2375ec5538982a5d475041c42
zhoujc <1258349367@qq.com>
2022-11-19 15:07:01 +0800

    feat：增加全局状态管理库redux

---
+++ src/store/modules/entire/reducer.js
@@ -0,0 +1,12 @@
+const initState = {
+  count: 5
+}
+
+const reducer = (state = initState, action) => {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
+
+export default reducer
---
+++ src/store/modules/home.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const homeSlice = createSlice({
+  name: "home",
+  initialState: {
+    message: '初始化消息'
+  },
+  reducers: {
+
+  }
+})
+
+export default homeSlice.reducer
